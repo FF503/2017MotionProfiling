7fd35d287033dfae5dceb14b0664d8d21ae4d71b
Ashwin Vangipuram <AVSurfer123@users.noreply.github.com>
2017-02-06 14:11:06 -0500

    Update RobotHardwarePracticeBot.java

--- src/org/usfirst/frc/team500/robot/RobotHardwarePracticeBot.java
+++ src/org/usfirst/frc/team500/robot/RobotHardwarePracticeBot.java
@@ -17,7 +17,7 @@ public class RobotHardwarePracticeBot extends RobotHardware {
 	
 	public final double WHEEL_DIAMETER = 4.0;
 	public final double WHEEL_BASE = 32.5;
-	public final double CYCLE_TIME = 0.05;
+	public final double CYCLE_TIME = 0.06;
 	
 	public final int COUNTS_PER_REV = 512;
 	public final double DRIVE_P = 0;
